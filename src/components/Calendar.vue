16e23e16a98b37922875365f3ed510c35508f567
jsonsinger <hello@jasonsinger.co.za>
2023-08-05 22:30:07 +0200

    Updated Greeting Message in Default layout based on the time.

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,48 +1,40 @@
 <script setup lang="ts">
 let current = new Date();
-//function dates(current: Date) {
-  let days = [];
-  // Starting Monday not Sunday
-  let sundayStart = 0;
-  let mondayStart = 1;
+let days = [];
+// Starting Monday not Sunday
+let sundayStart = 0;
+let mondayStart = 1;
 
 let startOfMonth = new Date(current.getFullYear(), current.getMonth(), 1);
 let endOfMonth = new Date(current.getFullYear(), current.getMonth() + 1, 0);
 
-  current.setDate((current.getDate() - current.getDay() + mondayStart));
-  for (let i = startOfMonth.getDate(); i < endOfMonth.getDate(); i++) {
-    days.push(new Date(current));
-    console.log(JSON.stringify(current.toLocaleDateString('en-ZA', { weekday:"long", month:"long", day:"numeric"})))
-    console.log(current.toLocaleDateString('en-ZA', { weekday:"short"}));
-    current.setDate(current.getDate() + 1);
-  }
-  //return week;
-//}
-let now = new Date().toLocaleDateString('en-ZA', { weekday:"long"});
-console.log(now)
-//console.log(dates(new Date()));
+current.setDate((current.getDate() - current.getDay() + mondayStart));
+for (let i = startOfMonth.getDate(); i < endOfMonth.getDate(); i++) {
+  days.push(new Date(current));
+  current.setDate(current.getDate() + 1);
+}
 </script>
 
 <template>
   <div class="w-full h-16 px-2 rounded border-dashed border-opacity-25 border-2 border-slate-300">
-      <section class="content">
+    <section class="content">
       <div class="scrolling-wrapper">
         <div class="card" v-for="day in days">
           <div>
             <span class="flex flex-col justify-center items-center text-gray-500">
-              <small>{{day.toLocaleDateString('en-ZA', { weekday:"short"})}} </small>
-              {{day.getDate()}}
+              <small>{{ day.toLocaleDateString('en-ZA', {weekday: "short"}) }} </small>
+              {{ day.getDate() }}
             </span>
           </div>
         </div>
       </div>
-      </section>
-    </div>
+    </section>
+  </div>
 </template>
 
 <style scoped>
 
-:root{
+:root {
   --days: 7;
 }
 
@@ -68,7 +60,8 @@ console.log(now)
   width: calc(100% / var(--days));
   height: 60px;
   background: ghostwhite;
-  :hover span{
+
+  :hover span {
     padding-left: 6px;
     padding-right: 6px;
     margin: 2px;
@@ -84,6 +77,7 @@ console.log(now)
   width: 100%;
   -webkit-overflow-scrolling: touch;
   touch-action: pan-x;
+
   &::-webkit-scrollbar {
     display: none;
   }
--- src/components/DefaultLayout.vue
+++ src/components/DefaultLayout.vue
@@ -1,16 +1,25 @@
 <script setup lang="ts">
-const today = new Date().toLocaleDateString('en-ZA', { weekday:"long", year:"numeric", month:"short", day:"numeric"})
+const today = new Date()
+const localeDateString = new Date().toLocaleDateString('en-ZA', {
+  weekday: "long",
+  year: "numeric",
+  month: "short",
+  day: "numeric"
+});
 </script>
 
 <template>
   <div class="flex flex-no-wrap bg-ghostwhite h-screen">
-<!--    <SidebarLayout/>-->
+    <!--    <SidebarLayout/>-->
     <div class="container mx-auto py-10 h-64 md:w-4/5 w-11/12 px-6">
       <div class="">
-        <h1>Hello</h1>
-        <h3 class="-mt-4 mb-4">{{ today }}</h3>
+        <h1 v-if="today.getHours() < 12">Good Morning</h1>
+        <h1 v-else-if="today.getHours() >= 12 && today.getHours() < 18">Good Afternoon</h1>
+        <h1 v-else-if="today.getHours() > 18 && today.getHours() < 21">Good Evening</h1>
+        <h1 v-else>Good Night</h1>
+        <h3 class="-mt-4 mb-4">{{ localeDateString }}</h3>
       </div>
-      <Calendar />
+      <Calendar/>
       <div class=" mt-4 w-full h-80 rounded border-dashed border-2 border-slate-300">
         <div class="wrapper">
 
